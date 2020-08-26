d829bb05971750af95fe7f79ee149c103a1b186f
Sergii Kulyk <saboteur@saboteur.com.ua>
2020-08-27 01:56:00 +0300

    initial working version

--- jks-bash-manager.sh
+++ jks-bash-manager.sh
@@ -1,90 +1,120 @@
-#!/usr/env bash
+#!/bin/ksh
 red=$(tput bold;tput setaf 1)
 green=$(tput bold;tput setaf 2)
 blue=$(tput bold;tput setaf 6)
-reset=$(tput sgr0)
+rst=$(tput sgr0)
+COLUMNS=1	# for select command
+STOREPASS="changeme"
+NL="
+"
+
+debug() {
+  echo "DEBUG: $1"
+}
+
+# $1: cert name, $2: keystore file
+delete_cert() {
+  echo -n "Write [${red}yes${rst}] or [${red}YES${rst}] to delete [${green}${1}${rst}] from ${green}${2}${rst}: "
+  read	
+  if [ "$REPLY" == yes -o "$REPLY" == YES ]; then
+    keytool -delete -alias "${1}" -keystore "${2}" -storepass ${STOREPASS}
+    echo "${red}Removing certificate [$1]${rst}"
+  else
+    echo "${red}Cancelled.${rst}"
+    return
+  fi
+
+  typeset -i cnt=2 next=3
+  found=0
+  while [ -n "${certName[$next]}" ]; do
+    [ "${certName[$cnt]}" == "${1}" ] && found=1
+    if [ $found == 1 ] ; then
+      certName[$cnt]=${certName[$next]}
+      certSerial[$cnt]=${certSerial[$next]}
+      certValid[$cnt]=${certValid[$next]}
+      certDays[$cnt]=${certDays[$next]}
+      certTitle[$cnt]=${certTitle[$next]}
+    fi
+    cnt+=1; next+=1
+  done
+  unset certTitle[$cnt]
+  unset certName[$cnt]
+}
 
 if [ -z "$1" ]; then
-  read -p "Provide keystore name (press ENTER to use default ${green}truststore.jks${reset}): "
+  echo -n "Provide keystore name (press ENTER to use default ${green}truststore.jks${rst}): "
+  read
   [ -z "$REPLY" ] && FILE="truststore.jks"
 else
   FILE="$1"
 fi
-STOREPASS="changeme"
 
-NL="
-"
-echo "Opening ${green}${FILE}${reset} ..."
+echo "Opening ${green}${FILE}${rst} ..."
 RESULT=$(keytool -list -v -keystore $FILE -storepass ${STOREPASS}|grep -P "(Alias name:|Serial number:|Valid from:)"|grep "Alias name:" -A 2)
 
-declare -A certificates
-declare -i counter=1
-alias=""
-serial=""
-valid=""
+# initialize cert list from keystore
+typeset -A certName
+typeset -A certSerial
+typeset -A certValid
+typeset -A certDays
+typeset -A certTitle
+typeset -i cnt=2
 while read; do
-  if [[ $REPLY =~ Alias ]]; then
-    alias=${REPLY##*: }
-  elif [[ $REPLY =~ Serial ]]; then
-    serial=${REPLY##*: }
+  if expr "$REPLY" : "Alias name: ">/dev/null; then
+    certName[$cnt]="${REPLY##*: }"
+  elif expr "$REPLY" : "Serial number: ">/dev/null; then
+    certSerial[$cnt]="${REPLY##*: }"
   else
-    valid=$(/bin/date --date="${REPLY##*until: }" "+%Y-%m-%d")
+    certValid[$cnt]=$(/bin/date --date="${REPLY##*until: }" "+%Y-%m-%d")
     validunix=$(/bin/date --date="${REPLY##*until: }" "+%s")
-    days=$(( (${validunix} - $(/bin/date "+%s")) / 3600 / 24 ))
-    certificates[$counter,0]="$alias"
-    certificates[$counter,1]="$serial"
-    certificates[$counter,2]="$valid"
-    certificates[$counter,3]="$days"
-    certs[$counter]=$(printf "%s %s\n" "${certificates[$counter,2]}" "${certificates[$counter,0]}")
-    counter+=1
+    certDays[$cnt]=$(( (${validunix} - $(/bin/date "+%s")) / 3600 / 24 ))
+    certTitle[$cnt]=$(printf "%s %s\n" "${certValid[$cnt]}" "${certName[$cnt]}")
+    cnt+=1
   fi
 done<<<"$RESULT"
 
-COLUMNS=1	# for select command
-echo "${NL}Keystore ${green}${FILE}${reset} list:"
+# main loop
+echo "${NL}Keystore ${green}${FILE}${rst} list:"
 while true; do
   PS3="${NL}Choose certificate entry: "
-  select item in "${certs[@]}" "${blue}exit${reset}"; do
-    if [ "$item" == "${blue}exit${reset}" ]; then
-      echo "${green}Good bye${reset}"
+  select item in "${blue}exit${rst}" "${certTitle[@]}"; do
+    if [ "$item" == "${blue}exit${rst}" ]; then
+      echo "${green}Good bye${rst}"
       exit 0
     fi
     ENTRY=$REPLY
-    echo ""
-    PS3="${NL}Choose action: "
-    select action in "Details" "Export" "Delete" "${blue}Back to list${reset}"; do
+    echo "Selected: ${green}${certName[$ENTRY]}${rst}${NL}"
+    PS3="${NL}Choose action for ${green}${certName[$ENTRY]}${rst}: "
+    select action in "${blue}Exit to list${rst}" "Details" "Export" "Delete"; do
       case $REPLY in
-        1) echo "${NL}Details for entry: [${green}${certificates[$ENTRY,0]}${reset}]"
-           echo "Serial number: ${certificates[$ENTRY,1]}"
-           echo "Valid to: ${certificates[$ENTRY,2]}"
-           echo "Days left: ${certificates[$ENTRY,3]}";;
-        2) echo "${NL}Exporting entry [${green}${certificates[$ENTRY,0]}${reset}]"
-           FILENAME=$(echo "${certificates[$ENTRY,0]}"|tr -d '[]()#*?\\/'|tr " " "_")
-           select format in "JKS" "PKCS12" "crt" "Cancel"; do
+        1) echo "${NL}Keystore ${green}${FILE}${rst} list:"
+           break 2;;
+        2) echo "${NL}Details for entry: [${green}${certName[$ENTRY]}${rst}]"
+           echo "Serial number: ${certSerial[$ENTRY]}"
+           echo "Valid to: ${certValid[$ENTRY]}"
+           echo "Days left: ${certDays[$ENTRY]}";;
+        3) PS3="${NL}Choose export format for ${green}${certName[$ENTRY]}${rst}: "
+           FILENAME=$(echo "${certName[$ENTRY]}"|tr -d '[]()#*?\\/'|tr " " "_")
+           select format in "JKS" "PKCS12" "crt" "${red}Cancel${rst}"; do
              case $REPLY in
-             1|2) FILENAME="$FILENAME.${format~~}"
-                read -p "Provide export file name (press ENTER to use: ${green}${FILENAME}${reset}) :"
-                [[ -n "$REPLY" ]] && FILENAME="$REPLY"
-                keytool -importkeystore -srckeystore "${FILE}" -destkeystore "${FILENAME}" -srcalias "${certificates[$ENTRY,0]}" -destalias "${certificates[$ENTRY,0]}" -srcstorepass ${STOREPASS} -deststorepass ${STOREPASS} -deststoretype ${format}
+             1|2) ext=$( echo $format | tr '[:upper:]' '[:lower:]')
+                FILENAME="$FILENAME.${ext}"
+                echo -n "Provide export file name (press ENTER to use: ${green}${FILENAME}${rst}) :"
+                read
+                [ -n "$REPLY" ] && FILENAME="$REPLY"
+                keytool -importkeystore -srckeystore "${FILE}" -destkeystore "${FILENAME}" -srcalias "${certName[$ENTRY]}" -destalias "${certName[$ENTRY]}" -srcstorepass ${STOREPASS} -deststorepass ${STOREPASS} -deststoretype ${format}
                 break;;
-             3) read -p "Provide export file name (press ENTER to use: ${green}${FILENAME}.crt${reset}) :"
-                keytool -exportcert -v -alias "${certificates[$ENTRY,0]}" -keystore "${FILE}" -storepass ${STOREPASS} -rfc -file "${FILENAME}.crt"
+             3) echo -n "Provide export file name (press ENTER to use: ${green}${FILENAME}.crt${rst}) :"
+                read
+                keytool -exportcert -v -alias "${certName[$ENTRY]}" -keystore "${FILE}" -storepass ${STOREPASS} -rfc -file "${FILENAME}.crt"
                 break;;
-             4) break;;
+             4) PS3="${NL}Choose action for ${green}${certName[$ENTRY]}${rst}: "
+                break;;
             esac
             done
-           echo "${green}Done.${reset}${NL}";;
-        3) read -p "Write [${red}yes${reset}] or [${red}YES${reset}] to delete [${green}${certificates[$ENTRY,0]}${reset}] from ${green}${FILE}${reset}: "
-           if [[ $REPLY =~ yes|YES ]]; then
-             echo "${red}deleting...${reset}"
-             #keytool -delete -alias "${certificates[$ENTRY,0]}" -keystore "${FILENAME}" -storepass ${STOREPASS}
-           else
-             echo "${red}Cancelled.${reset}"
-           fi
-           echo "${NL}Keystore ${green}${FILE}${reset} list:"
-           break 2;;
-        4) echo ""
-           echo "${NL}Keystore ${green}${FILE}${reset} list:"
+           echo "${green}Done.${rst}${NL}";;
+        4) delete_cert "${certName[$ENTRY]}" "${FILE}"
+           echo "${NL}Keystore ${green}${FILE}${rst} list:"
            break 2;;
       esac
     done
